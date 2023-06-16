0dcf847e0154f2348210a874886102ff066f2377
husayn07 <huzain270@gmail.com>
2023-06-15 20:46:57 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - c programmbfor size of data
+ * Return: 0 good
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+printf("size of a char: %lu byte(d)\n", (unsigned long)sizeof(a));
+printf("size of a int: %lu byte(d)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(d)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(d)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(d)\n", (unsigned long)sizeof(e));
+return (0);
+}
