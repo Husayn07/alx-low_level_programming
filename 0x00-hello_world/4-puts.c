3cfe4ebb3a41b4956f9d4118b65b805d040346db
husayn07 <huzain270@gmail.com>
2023-06-15 19:33:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include < stdio.h >
+/**
+ * main - Main to write a code that print a text using a put function
+ * Return: 0
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
