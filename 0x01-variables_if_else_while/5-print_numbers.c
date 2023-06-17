bf7dc90d951a9284ab1effd7096173dccc89b0f8
husayn07 <huzain270@gmail.com>
2023-06-16 22:02:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A main fuction thay delcrea data type
+ * loop - a loop fuction
+ * Return: this return zero
+ */
+int main(void)
+{
+	char A;
+
+	A = 0;
+	while
+		(A <= 9) {
+			putchar(A);
+			A++;
+		}
+	putchar('\n');
+	return (0);
+}
