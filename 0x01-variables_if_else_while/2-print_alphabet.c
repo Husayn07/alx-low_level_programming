4e3240e6cf5c25a1da2182a6a03735aca68d0edf
husayn07 <huzain270@gmail.com>
2023-06-16 21:32:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A main fuction thay delcrea char data type
+ * loop - a loop fuction
+ * Return: this return zero
+ */
+int main(void)
+{
+	char A;
+
+	A = 'a';
+	while
+		(A <= 'z') {
+			putchar(A);
+			A++;
+		}
+	putchar("\n");
+	return (0);
+}
