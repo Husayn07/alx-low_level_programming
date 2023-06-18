6fa918714986b815fbc855faec7e0a0f13f19589
husayn07 <huzain270@gmail.com>
2023-06-18 12:14:17 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - to generate 4 for  loops for varrying 4 int
+ * if included - to streamline the standard output
+ * Return: 0 succeded
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (c < a || d <= b)
+					{
+						continue;
+					}
+					putchar(a + 48);
+					putchar(b + 48);
+					putchar(' ');
+					putchar(c + 48);
+					putchar(d + 48);
+					if (b == 8)
+					{
+						continue;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
