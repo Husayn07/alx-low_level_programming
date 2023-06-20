9b88dab5d0d4bd73b74ffbc0827b62a4eaeda506
husayn07 <huzain270@gmail.com>
2023-06-20 12:26:36 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - generate a loop fo a -z
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int A;
+	int B;
+
+	for (B = 1; B <= 10; B++)
+	{
+		for (A = 'a'; A <= 'z'; A++)
+		{
+			_putchar(A);
+			_putchar('\n');
+		}
+	}
+}
