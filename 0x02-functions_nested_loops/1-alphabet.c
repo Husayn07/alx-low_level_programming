75c6d00226b47e0fea46bcbdd6d4fbcd658e99a3
husayn07 <huzain270@gmail.com>
2023-06-20 12:06:04 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet -: generate a loop fo a -z
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		_putchar('A');
+	}
+	_putchar('\n');
+}
