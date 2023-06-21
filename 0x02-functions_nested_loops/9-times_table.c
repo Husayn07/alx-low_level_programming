a308499a0dcd0d0c9211ec9163d3589000915cbe
husayn07 <huzain270@gmail.com>
2023-06-21 04:48:46 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,22 +9,24 @@ void times_table(void)
 {
 	int a;
 	int b;
-	int c;
 
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar('0');
 		for (b = 0; b <= 59; b++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			int c;
+
 			c = a * b;
-			if (c <= 9)
-			_putchar(' ');
-			else
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
+			if (b == 9)
+			{
+				continue;
+			}
+			_putchar(',');
+			_putchar(' ');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
