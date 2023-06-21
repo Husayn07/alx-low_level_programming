22350de7b7c24ad6c1829f046f86118d440c9cd7
husayn07 <huzain270@gmail.com>
2023-06-21 03:25:43 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - to generate for  loops for varrying 9
+ * Return: 0 succeded
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			c = a * b;
+			if (c <= 9)
+			{
+				_putchar(' ');
+			}
+			else 
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');	
+	}
+}
