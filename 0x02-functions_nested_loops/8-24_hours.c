73e64809d0d7744fac5ff0c8222bd6fed7dc634c
husayn07 <huzain270@gmail.com>
2023-06-20 21:58:19 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - to generate 4 for  loops for varrying 3 int
+ *
+ * Return: 0 succeded
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(':');
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
