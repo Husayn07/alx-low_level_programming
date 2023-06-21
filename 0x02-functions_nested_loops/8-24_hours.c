6d2d16df436e10ca53e0240dd0519c10523433f6
husayn07 <huzain270@gmail.com>
2023-06-21 02:07:20 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,28 +10,17 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0;b <= 59; b++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a == 2 && b == 3 && c == 5 && d == 9)
-					{
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(':');
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
-					}
-				}
-			}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + 'p');;
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
