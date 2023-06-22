b4c6c689bc5a7c1f691e81781b601ecbbae527ac
husayn07 <huzain270@gmail.com>
2023-06-22 08:25:51 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - generate a loop for 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int A;
+
+	for (A = '0'; A <= '9'; A++)
+	{
+		_putchar(A);
+		if ((A == '2') || (A == '4')
+				{
+				continue;
+				}
+	}
+	_putchar('\n');
+}
