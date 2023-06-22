b87ec96f7509da023b498095fcb194311277045e
husayn07 <huzain270@gmail.com>
2023-06-22 07:35:27 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function thay check for digit
+ * @c: parameter
+ * Return: 1 when c is lowwr case else 0
+ */
+
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Returns 1 if c is a letter, lowercase or uppercase
+ * @c: parameter
+ * Return: 1 when c is lowwr case else 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
