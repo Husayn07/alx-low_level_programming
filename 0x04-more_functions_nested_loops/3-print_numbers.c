5d940aa6aa1bc9a62d5a9b1ae6e03d2661404428
husayn07 <huzain270@gmail.com>
2023-06-22 22:41:42 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_numbers - generate a loop for 0 to 9
+ * Return: void
+ */
+void main()
+{
+	int A;
+
+	for (A = 48; A <= 57; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+}
