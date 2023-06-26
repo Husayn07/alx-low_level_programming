55c08a84d06244201a8ffdb316fe170efb8b12b6
husayn07 <huzain270@gmail.com>
2023-06-26 08:47:12 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,15 +4,14 @@
  * swap_int - function to change the vale of it pointer
  * @a: parameter
  * @b: parameter
- * @m: parameter
  *Return: void
  */
 
 void swap_int(int *a, int *b)
 {
-	        int m;
-		
-		m = *a;
-		*a = *b;
-		*b = m;
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
 }
