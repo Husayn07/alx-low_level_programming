93ea09f405e5fac514ccb26ab5fc98032afa9d2d
husayn07 <huzain270@gmail.com>
2023-06-29 09:07:31 -0700

    all

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-	
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -22,5 +22,6 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
