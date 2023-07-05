38949cbe593e233633b63369668d14412ef82302
husayn07 <huzain270@gmail.com>
2023-07-05 01:24:28 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,15 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s = '\0')
+	{
+		_putchar('\n');
+		break;
+	}
+	else
 	{
 		_putchar(*s);
 		++s;
+		_puts_recursion(char *s);
 	}
-	_putchar('\n');
 }
