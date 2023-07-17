5b68ce6f0c4a87e55f8333c919b85b57bf7c3fd6
husayn07 <huzain270@gmail.com>
2023-07-17 07:13:44 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,7 +5,7 @@
  *main - Function that prints the name of the files it was compile from
  *Return: Always success (0)
  */
-int main(void) 
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
