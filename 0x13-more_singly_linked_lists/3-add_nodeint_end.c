bc8ec3012e0a22e207848b6e9198041fc96a7f2c
husayn07 <huzain270@gmail.com>
2023-08-01 07:44:40 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,10 +8,8 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *ptr;
-	listint_t *template;
-
-	(void)template;
+	listint_t *ptr = NULL;
+	listint_t *template = NULL;
 
 	ptr = malloc(sizeof(listint_t));
 
