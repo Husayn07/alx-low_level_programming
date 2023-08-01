eef87b3809431f2ce778d5eda9e69ffa73a44643
husayn07 <huzain270@gmail.com>
2023-08-01 08:21:54 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,10 +9,9 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	ptr = head;
-	while (ptr)
+	while ((ptr = head) != NULL)
 	{
-		ptr = ptr->next;
+		head = head->next;
 		free(ptr);
 	}
 }
